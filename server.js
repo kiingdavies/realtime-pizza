95f0e6c2398a9beb93ac18ab10bdda9ad0a18205
Adedeji Davies <daviesadedeji007.com>
2021-01-09 22:11:29 +0100

    Part 4 & 5 Done

--- server.js
+++ server.js
@@ -8,15 +8,30 @@ const PORT = process.env.PORT || 3300;
 // Assets
 app.use(express.static('public'));
 
-// Get Home page
-app.get('/', (req, res)=> {
-    res.render("home");
-});
-
 // set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+// Get Home Page
+app.get('/', (req, res)=> {
+    res.render("home");
+});
+
+// Get Cart Page
+app.get('/cart', (req, res)=>{
+    res.render('customers/cart');
+});
+
+// Get Login Page
+app.get('/login', (req,res)=>{
+    res.render('auth/login');
+});
+
+// Get Register Page
+app.get('/register', (req, res)=> {
+    res.render('auth/register');
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server is listening on port ${PORT}`);
